cfd4777c042bb1f3b7ab20f14b9519411e155365
Ngeng Chhengkim <chhengkimngeng@gmail.com>
2015-05-12 16:06:25 +0700

    sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,24 @@ class UsersController < ApplicationController
   def show
     @user = User.find params[:id]
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new params_users
+    if @user.save
+      flash.now[:success] = "Account sign up successful"
+      redirect_to @user     
+    else
+      render "new"    
+    end
+  end
+
+  private
+  def params_users
+    params.require(:user).permit :username, :email, :password, 
+                                  :password_confirmation
+  end
 end

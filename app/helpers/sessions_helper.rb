fd740f4b129704757471fcc3f66118c1e063e834
Ngeng Chhengkim <chhengkimngeng@gmail.com>
2015-05-15 13:00:28 +0700

    log out

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -30,4 +30,11 @@ module SessionsHelper
     cookies.delete :user_id
     cookies.delete :remember_token
   end
+
+  def log_out
+    forget current_user
+    session.delete :user_id
+    @current_user = nil
+  end
+
 end

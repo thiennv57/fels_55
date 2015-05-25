d4be8c5e39c90eb1ee5ccce8a6fe124b46cdc753
yongsokheng <yongsokheng@ymail.com>
2015-05-25 16:13:32 +0700

    Design Home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
+    if logged_in?
+      @lessons = Lesson.filter_lesson(current_user.id).paginate page: params[:page]
+    end
   end
 end
